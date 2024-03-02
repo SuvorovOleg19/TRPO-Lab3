64b081533cc9c4908d5d9ae397b1246b05a384fb
SuvorovOleg19 <145707292+SuvorovOleg19@users.noreply.github.com>
2024-03-02 14:58:47 +0500

    Добавлен первый тест

--- TRPO-Lab3.Tests/UnitTest1.cs
+++ TRPO-Lab3.Tests/UnitTest1.cs
@@ -1,16 +1,20 @@
+using TRPO_Lab3.Library;
+
 namespace TRPO_Lab3.Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void AddTest()
         {
-            Assert.Pass();
+            const float p = 50;
+            const float a = 3;
+            const float expected = 75;
+
+            var result = 0.5 * p * a; ;
+
+            Assert.AreEqual(expected, result);
         }
     }
 }
