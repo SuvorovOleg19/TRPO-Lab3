4a673268839a5cae347449b1ffe6b232c1fd38f3
SuvorovOleg19 <145707292+SuvorovOleg19@users.noreply.github.com>
2024-03-10 13:32:18 +0500

    Добавление файла «.gitlab-ci.yml»
    Также усовершенствования кодов приложение и тестов

--- TRPO-Lab3.ConsoleApp/Program.cs
+++ TRPO-Lab3.ConsoleApp/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Введите периметр основания правильной пирамиды : ");
+        Console.WriteLine("Введите периметр основания правильной пирамиды: ");
         double p = float.Parse(Console.ReadLine());
-        Console.WriteLine("Введите апофема правильной пирамиды: ");
+        Console.WriteLine("Введите апофему правильной пирамиды: ");
         double a = float.Parse(Console.ReadLine());
         Console.WriteLine("Полученная боковая поверхность правильной пирамиды: ");
         double s = Lateral_Surface_Regular_Piramid.Lateral_Surface_Regular_Piramid_formula(p, a);
